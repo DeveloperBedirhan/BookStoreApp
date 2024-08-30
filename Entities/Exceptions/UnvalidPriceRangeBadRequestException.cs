87316b2f761338725529b3ad9ea1646b717f6302
Bedirhan İleri <77440477+DeveloperBedirhan@users.noreply.github.com>
2024-08-30 16:34:43 +0300

    Add files via upload

---
+++ Entities/Exceptions/UnvalidPriceRangeBadRequestException.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities.Exceptions
+{
+    public class UnvalidPriceRangeBadRequestException : BadRequestException
+    {
+        public UnvalidPriceRangeBadRequestException() 
+            : base("Max price should be greater than min price.") 
+        {
+            
+        }
+    }
+}
